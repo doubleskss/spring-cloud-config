54e0d939df32686d2b762cfc8c858d0b32ed60d0
doubleskss <kss8045@naver.com>
2020-10-04 14:50:39 +0900

    Create JasyptConfig.java

---
+++ config-server/src/main/java/com/example/app/JasyptConfig.java
@@ -0,0 +1,38 @@
+package com.example.app;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Jasypt 설정 클래스
+ *
+ * Created by mhlab(dex) on 2019-03-06.
+ */
+
+
+@Configuration
+public class JasyptConfig {
+
+    /**
+     * 설정 빈
+     * @return
+     */
+    @Bean("jasyptStringEncryptor")
+    public StringEncryptor stringEncryptor() {
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        config.setPassword("movillapp"); //암호화에 사용할 키 -> 중요
+        config.setAlgorithm("PBEWithMD5AndDES"); //사용할 알고리즘
+        config.setKeyObtentionIterations("1000");
+        config.setPoolSize("1");
+        config.setProviderName("SunJCE");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setStringOutputType("base64");
+        encryptor.setConfig(config);
+        return encryptor;
+    }
+
+}
